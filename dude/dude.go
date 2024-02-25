83ed2a7b6ca80d21637f3628a68fcba9133dab63
Michael Thorsager <thorsager@gmail.com>
2024-02-25 21:39:17 +0100

    Minor cleanup, messing around

---
+++ dude/dude.go
@@ -0,0 +1,167 @@
+package dude
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"github.com/thorsager/dude/persistence"
+	"github.com/thorsager/dude/requestid"
+	"log"
+	"net/http"
+)
+
+type Dude struct {
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Phrase string `json:"phrase"`
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	rid := requestid.GetID(ctx)
+	var d Dude
+	err := json.NewDecoder(r.Body).Decode(&d)
+	if err != nil {
+		log.Printf("[%s] could not decode dude: %v", rid, err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		log.Printf("[%s] could not prepare statement: %v", rid, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	db := persistence.GetConnection(ctx)
+
+	id := 0
+	err = db.QueryRowContext(ctx, "INSERT INTO dudes (name, phrase) VALUES ($1, $2) RETURNING id", d.Name, d.Phrase).Scan(&id)
+	if err != nil {
+		log.Printf("[%s] could not execute statement: %v", rid, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	d.ID = id
+	err = json.NewEncoder(w).Encode(d)
+	if err != nil {
+		log.Printf("[%s] could not encode dude: %v", rid, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func GetById(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	ctx := r.Context()
+	rid := requestid.GetID(ctx)
+	db := persistence.GetConnection(ctx)
+	var d Dude
+	row := db.QueryRowContext(ctx, "SELECT id, name, phrase FROM dudes WHERE id = $1", id)
+	err := row.Scan(&d.ID, &d.Name, &d.Phrase)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			http.NotFound(w, r)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(d)
+	if err != nil {
+		log.Printf("[%s] could not encode dudes: %v", rid, err)
+	}
+}
+
+func GetAll(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	rid := requestid.GetID(ctx)
+	db := persistence.GetConnection(ctx)
+	rows, err := db.QueryContext(ctx, "SELECT id, name, phrase FROM dudes ORDER BY id")
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			http.NotFound(w, r)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var alldudes []Dude
+	for rows.Next() {
+		var d Dude
+		err := rows.Scan(&d.ID, &d.Name, &d.Phrase)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		alldudes = append(alldudes, d)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(alldudes)
+	if err != nil {
+		log.Printf("[%s] could not encode dudes: %v", rid, err)
+	}
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	rid := requestid.GetID(ctx)
+	var d Dude
+	err := json.NewDecoder(r.Body).Decode(&d)
+	if err != nil {
+		log.Printf("[%s] could not decode dude: %v", rid, err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	db := persistence.GetConnection(ctx)
+
+	result, err := db.ExecContext(ctx, "UPDATE dudes SET name=$2, phrase=$3 WHERE id = $1", d.ID, d.Name, d.Phrase)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			http.NotFound(w, r)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	affected, err := result.RowsAffected()
+	if err != nil {
+		log.Printf("[%s] could not determine rows affected: %v", rid, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	if affected == 0 {
+		log.Printf("[%s] no rows affected", rid)
+	}
+	w.WriteHeader(http.StatusNoContent)
+	return
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	rid := requestid.GetID(ctx)
+	id := r.PathValue("id")
+	db := persistence.GetConnection(ctx)
+	result, err := db.ExecContext(ctx, "DELETE FROM dudes WHERE id = $1", id)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			http.NotFound(w, r)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	affected, err := result.RowsAffected()
+	if err != nil {
+		log.Printf("[%s] could not determine rows affected: %v", rid, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if affected == 0 {
+		log.Printf("[%s] no rows affected", rid)
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
--- main.go
+++ main.go
@@ -1,33 +1,29 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
-	"errors"
+	"context"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/thorsager/dude/dude"
 	"github.com/thorsager/dude/metricsandlogging"
 	"github.com/thorsager/dude/middleware"
 	"github.com/thorsager/dude/persistence"
 	"github.com/thorsager/dude/requestid"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
+	"os/signal"
 	"strings"
 )
 
 const EnvDbPrefix = "DB_"
 const EnvUrlSuffix = "_URL"
 
-type Dude struct {
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	Phrase string `json:"phrase"`
-}
-
 var defaultDataBase = ""
+var BindAddress = ":8080"
 
 var handler = middleware.Compose(
 	func(handlerFunc http.HandlerFunc) http.HandlerFunc {
@@ -65,6 +61,10 @@ func readEnvironment() ([]persistence.NamedUrl, error) {
 }
 
 func main() {
+	// handle SIGINT
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
 	err := godotenv.Load()
 	if err != nil && !os.IsNotExist(err) {
 		log.Fatal("Error loading .env file")
@@ -87,159 +87,43 @@ func main() {
 		defaultDataBase = nurls[0].Name
 	}
 
+	serveMux := http.NewServeMux()
+
 	// Register the metrics handler
-	http.Handle("/metrics", promhttp.Handler())
+	serveMux.Handle("/metrics", promhttp.Handler())
 
-	http.HandleFunc("POST /dude", handler(createDude))
-	http.HandleFunc("GET /dude", handler(getAllDudes))
-	http.HandleFunc("GET /dude/{id}", handler(getDudeById))
-	http.HandleFunc("PUT /dude", handler(updateDude))
-	http.HandleFunc("DELETE /dude/{id}", handler(deleteDude))
+	serveMux.HandleFunc("POST /dude", handler(dude.Create))
+	serveMux.HandleFunc("GET /dude", handler(dude.GetAll))
+	serveMux.HandleFunc("GET /dude/{id}", handler(dude.GetById))
+	serveMux.HandleFunc("PUT /dude", handler(dude.Update))
+	serveMux.HandleFunc("DELETE /dude/{id}", handler(dude.Delete))
 
-	log.Printf("Server starting on port 8080")
-	err = http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err)
+	srv := &http.Server{
+		Addr:        BindAddress,
+		BaseContext: func(_ net.Listener) context.Context { return ctx },
+		Handler:     serveMux,
 	}
-}
 
-func createDude(w http.ResponseWriter, r *http.Request) {
-	var d Dude
-	err := json.NewDecoder(r.Body).Decode(&d)
-	if err != nil {
-		log.Printf("could not decode dude: %v", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	// Start the server
+	srvErr := make(chan error, 1)
+	go func() {
+		log.Printf("Server starting on port %s", BindAddress)
+		srvErr <- srv.ListenAndServe()
+	}()
+
+	// Wait for either an error or a signal
+	select {
+	case err = <-srvErr:
+		log.Fatalf("while starting: %s", err)
 		return
-	}
-	if err != nil {
-		log.Printf("could not prepare statement: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	case <-ctx.Done():
+		stop() // stop listening for SIGINT
 	}
 
-	ctx := r.Context()
-	db := persistence.GetConnection(ctx)
-
-	id := 0
-	err = db.QueryRowContext(ctx, "INSERT INTO dudes (name, phrase) VALUES ($1, $2) RETURNING id", d.Name, d.Phrase).Scan(&id)
+	// Shutdown the server
+	log.Printf("Shutting down server")
+	err = srv.Shutdown(context.Background())
 	if err != nil {
-		log.Printf("could not execute statement: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		log.Fatalf("while shutting down: %s", err)
 	}
-
-	d.ID = id
-	err = json.NewEncoder(w).Encode(d)
-	if err != nil {
-		log.Printf("could not encode dude: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-func getDudeById(w http.ResponseWriter, r *http.Request) {
-	id := r.PathValue("id")
-	ctx := r.Context()
-	db := persistence.GetConnection(ctx)
-	var d Dude
-	row := db.QueryRowContext(ctx, "SELECT id, name, phrase FROM dudes WHERE id = $1", id)
-	err := row.Scan(&d.ID, &d.Name, &d.Phrase)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			http.NotFound(w, r)
-			return
-		}
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(d)
-	if err != nil {
-		log.Printf("could not encode dudes: %v", err)
-	}
-}
-
-func getAllDudes(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	db := persistence.GetConnection(ctx)
-	rows, err := db.QueryContext(ctx, "SELECT id, name, phrase FROM dudes ORDER BY id")
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			http.NotFound(w, r)
-			return
-		}
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var allDdudes []Dude
-	for rows.Next() {
-		var d Dude
-		err := rows.Scan(&d.ID, &d.Name, &d.Phrase)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		allDdudes = append(allDdudes, d)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(allDdudes)
-	if err != nil {
-		log.Printf("could not encode dudes: %v", err)
-	}
-}
-
-func updateDude(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	var d Dude
-	err := json.NewDecoder(r.Body).Decode(&d)
-	if err != nil {
-		log.Printf("could not decode dude: %v", err)
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	db := persistence.GetConnection(ctx)
-
-	result, err := db.ExecContext(ctx, "UPDATE dudes SET name=$2, phrase=$3 WHERE id = $1", d.ID, d.Name, d.Phrase)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			http.NotFound(w, r)
-			return
-		}
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	_, err = result.RowsAffected()
-	if err != nil {
-		log.Printf("could not determine rows affected: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	w.WriteHeader(http.StatusNoContent)
-	return
-}
-
-func deleteDude(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	id := r.PathValue("id")
-	db := persistence.GetConnection(ctx)
-	result, err := db.ExecContext(ctx, "DELETE FROM dudes WHERE id = $1", id)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			http.NotFound(w, r)
-			return
-		}
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	affected, err := result.RowsAffected()
-	if err == nil {
-		log.Printf("deleted %d dudes", affected)
-	} else {
-		log.Printf("could not determine rows affected: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusNoContent)
 }
